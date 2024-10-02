432cf7a24846b39aea66b0150bab9a4288d9ea4b
gregoryput <gregorysanchez232@gmail.com>
2024-10-02 11:16:02 -0400

    login arreglando estilo

--- src/page/login/index.jsx
+++ src/page/login/index.jsx
@@ -1,7 +1,7 @@
 
 import { Mod, MultIdioma } from "@/hook/Config";
 import "./login.css"
-import { Github, Moon, Sun } from "lucide-react"
+import { Github, Linkedin, Moon, Sun } from "lucide-react"
 import { Button, Card, Form, Input } from "antd";
 import img from "../../assets/logo.jpeg";
 
@@ -20,19 +20,19 @@ export default function Login() {
     <div className="container-login">
       <div className="nav-login">
         <div className="box-container-align">
-          <img src={img} alt="" style={{width:20,height:20}} />
-          <p style={{marginLeft:10,fontWeight:"bold", marginRight:20}}>Progreso</p>
+          <img src={img} alt="" style={{ width: 20, height: 20 }} />
+          <p style={{ marginLeft: 10, fontWeight: "bold", marginRight: 20 }}>Progreso</p>
           <span>|</span>
-          <p style={{marginLeft:10}}>Version 1.0.0</p>
+          <p style={{ marginLeft: 10 }}>Version 1.0.0</p>
         </div>
-       <div className="box-container-align">
-       <button onClick={() => toggleIdioma(!Idioma)} className="button-login">
-          {Idioma == true ? <b style={{ color: `${isActive == false ? "white" : "black"}` }}>ES</b> : <b style={{ color: `${isActive == false ? "white" : "black"}` }}>EN</b>}
-        </button>
-        <button onClick={() => toggleActive(!isActive)} className={`button-login ${isActive === false ? "dark" : ""}`}>
-          {isActive == true ? <Sun /> : <Moon />}
-        </button>
-       </div>
+        <div className="box-container-align">
+          <button onClick={() => toggleIdioma(!Idioma)} className="button-login">
+            {Idioma == true ? <b style={{ color: `${isActive == false ? "white" : "black"}` }}>ES</b> : <b style={{ color: `${isActive == false ? "white" : "black"}` }}>EN</b>}
+          </button>
+          <button onClick={() => toggleActive(!isActive)} className={`button-login ${isActive === false ? "dark" : ""}`}>
+            {isActive == true ? <Sun /> : <Moon />}
+          </button>
+        </div>
 
       </div>
       <div className="form-container">
@@ -82,7 +82,7 @@ export default function Login() {
 
             <Form.Item
             >
-              <Button type="primary" style={{ width: "100%", height: "40px" ,marginTop: "30px"}} htmlType="submit">
+              <Button type="primary" style={{ width: "100%", height: "40px", marginTop: "30px" }} htmlType="submit">
                 Iniciar
               </Button>
             </Form.Item>
@@ -97,10 +97,16 @@ export default function Login() {
         </Card>
       </div>
 
-      <div onClick={()=>{ window.location.href = 'https://github.com/gregoryput';}} className="btn-credi">
-        <Github/>
-        <p style={{marginLeft:10}}>Gregoryput</p>
-      </div>
+      <footer className="footer">
+        <p style={{ marginLeft: 10 , fontWeight:"lighter", cursor:"default", fontSize:13 }}>Developed by</p>
+
+        <div onClick={() => { window.location.href = 'https://github.com/gregoryput'; }} className="btn-credi">
+          <Github width={20} />
+        </div>
+        <div onClick={() => { window.location.href = 'https://www.linkedin.com/in/gregory-albert-s%C3%A1nchez-05820019b/'; }} className="btn-credi">
+          <Linkedin  width={20} />
+        </div>
+      </footer>
     </div>
   )
 }
